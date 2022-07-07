bcd785ac0116e88ebb86c17febbd8e7023717ee7
TecHeReTiC <talex73@mail.ru>
2022-07-07 05:41:20 +0500

    Initial commit

---
+++ scripts/openGL_basics.py
@@ -0,0 +1,17 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import pygame
+
+pygame.init()
+
+vertexes = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (1, 1, 1),
+    (1, 1, 1),
+    (1, 1, 1),
+    (1, 1, 1),
+    (1, 1, 1),
+
+)
